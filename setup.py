3ee5b184b2c8a4b6b4a9934489652761115c3783
pkapgatelive <59923992+pkapgatelive@users.noreply.github.com>
2023-04-25 20:07:44 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Learning the MLOps with the wafer project ',
+    author='Pappu Kapgate',
+    license='MIT',
+)
